828993fd97356885fc552a848d05dd7d2b63c7dd
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-02-06 11:38:43 +0300

    Time: 71 ms (60.40%), Space: 15 MB (70.84%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -4,18 +4,17 @@ class Solution:
         pointer1=0
         pointer2=len(nums)-1
         for i in range(len(nums)):
-            if pointer1<len(nums) and pointer2<len(nums):
-                summ=temp[pointer1]+temp[pointer2]
-                if summ==target:
-                    if temp[pointer1] == temp[pointer2]:
-                        loc=nums.index(temp[pointer1])
-                        nums.pop(loc)
-                        return[loc,(nums.index(temp[pointer1])+1)]
-                    return  [nums.index(temp[pointer1]),nums.index(temp[pointer2])]
-                elif summ<target:
-                    pointer1=pointer1+1
-                elif summ>target:
-                    pointer2=pointer2-1
+            summ=temp[pointer1]+temp[pointer2]
+            if summ==target:
+                if temp[pointer1] == temp[pointer2]:
+                    loc=nums.index(temp[pointer1])
+                    nums.pop(loc)
+                    return[loc,(nums.index(temp[pointer1])+1)]
+                return  [nums.index(temp[pointer1]),nums.index(temp[pointer2])]
+            elif summ<target:
+                pointer1=pointer1+1
+            elif summ>target:
+                pointer2=pointer2-1
     
         
         
