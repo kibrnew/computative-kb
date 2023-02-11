38b0117d40f7c7006421fa90e0b2634f9fc447f2
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-02-11 22:14:58 +0300

    Time: 122 ms (98.27%), Space: 14.8 MB (71.38%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -3,17 +3,17 @@ class Solution:
         pr1=0
         pr2=0
         store={'a','e','i','o','u'}
-        ans=[]
+        ans=set([])
         count=0
         for j in range(k):
             if s[j] in store:
                 count+=1
-        ans.append(count)
+        ans.add(count)
         for i in range(k,len(s)):
             if s[i] in store:
                 count+=1
             if s[i-k] in store:
                 count-=1
-            ans.append(count)
+            ans.add(count)
             
         return max(ans)
