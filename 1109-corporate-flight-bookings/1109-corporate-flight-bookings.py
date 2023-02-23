75d2da9cde2b81654b4e396934e062de93f97b8e
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-02-23 10:47:42 +0300

    Time: 908 ms (50.95%), Space: 28.3 MB (32.13%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.py
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.py
@@ -1,21 +1,18 @@
 class Solution:
     def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
-        #time_complexity = o(n)
-        #space complexity = o(n)
-        
-        #intialize a list to perform prefix sum on the intervals
-        prefix = [0]*(n+1)
-        
-        #iterate to each book and assign to their correspondence interval
-        for book in bookings:
-            prefix[book[0]-1]+=book[2]
-            prefix[book[1]]-=book[2]
-        
-        #apply prefix sum on the assigned interval to get the total sum on each interval
-        tot = 0
-        for i,pre in enumerate(prefix):
-            tot+=pre
-            prefix[i]=tot
-            
-        #return the answer upto n 
-        return prefix[0:n]
+        prefix = [0] * (n + 1)
+
+        # iterate over each booking and assign it to its corresponding interval
+        for booking in bookings:
+            start, end, seats = booking
+            prefix[start - 1] += seats
+            prefix[end] -= seats
+
+        # apply prefix sum on the assigned interval to get the total sum on each interval
+        total_seats = 0
+        for i in range(n + 1):
+            total_seats += prefix[i]
+            prefix[i] = total_seats
+
+        # return the answer up to n
+        return prefix[:n]
