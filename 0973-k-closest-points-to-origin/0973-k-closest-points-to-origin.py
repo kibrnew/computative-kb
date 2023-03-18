064ed3e37f509abd976aa643542a46831be19d6e
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-03-18 14:59:04 +0300

    Time: 790 ms (81.71%), Space: 20.5 MB (19.82%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,15 +1,20 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        d=defaultdict(bool)
         temp=[]
         for i in range(len(points)):
             w=((points[i][0])**2)+((points[i][1])**2)
-            temp.append((w,i))
-        print(temp)
+            temp.append(w)
+            if not d[w]:
+                d[w]=[points[i]]
+            else :
+                d[w].append(points[i])
         temp.sort()
         ans=[]
         for j in range(k):
-            ind=temp[j][1]
-            ans.append(points[ind])
+            ind=d[temp[j]].pop()
+            ans.append(ind)
+            
         return ans
             
             
