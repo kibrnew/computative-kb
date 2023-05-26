70b39ea98b6fde8b81412c2795654161d0996dc0
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-05-26 15:20:49 +0300

    Time: 81 ms (30.37%), Space: 16.3 MB (29.50%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,28 +5,19 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        temp1=[]
-        temp2=[]
-        while l1.next:
-            temp1.append(str(l1.val))
-            l1=l1.next
-        temp1.append(str(l1.val))
-        l1=l1.next
-        while l2.next:
-            temp2.append(str(l2.val))
-            l2=l2.next
-        temp2.append(str(l2.val))
-        l2=l2.next
-        
-        ans1=int("".join(temp1[::-1]))
-        ans2=int("".join(temp2[::-1]))
-        ans=str(ans1+ans2)[::-1]
-        new0=ListNode(int(ans[0]))
-        head=new0
-        for i in ans[1:]:
-            new=ListNode(int(i))
-            new0.next=new
-            new0=new
-        return head
+        head=ListNode()
+        temp=head
+        carry=0
+        while l1 or l2 or carry:
+            n1=l1.val if l1 else 0
+            n2=l2.val if l2 else 0
+            s=n1+n2+carry
+            carry=s//10
+            s=s%10
+            temp.next=ListNode(s)
+            temp=temp.next
+            l1=l1.next if l1 else None 
+            l2=l2.next if l2 else None
+        return head.next
         
         
