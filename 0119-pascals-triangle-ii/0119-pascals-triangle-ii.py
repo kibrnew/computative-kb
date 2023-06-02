88896e6ba3c4b2e9fd6d1bfe6fe8a097c1513247
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-06-02 18:10:18 +0300

    Time: 37 ms (61.48%), Space: 16.4 MB (11.88%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -1,13 +1,11 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
         def pascal(l):
-            if rowIndex<=1:
-                return [1]*(rowIndex+1)
             if len(l)==rowIndex+1:
                 return l
             for i in range(len(l)-1):
                 l[i]=l[i]+l[i+1]
             l.insert(0,1)
             return pascal(l)
-        return pascal([1,2,1])
+        return pascal([1])
                 
