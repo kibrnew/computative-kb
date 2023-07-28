ad225c58b24c027134df4ad0a45d90aef1c2fa4c
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-07-28 12:18:14 +0300

    Time: 29 ms (99.75%), Space: 16.3 MB (58.43%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        
+        def preorder(root,l):
+            if not root:
+                l.append(None)
+                return l 
+            l.append(root.val)
+            l=preorder(root.left,l)
+            l=preorder(root.right,l)
+            return l
+        def inorder(root,l):
+            if not root:
+                l.append(None)
+                return l 
+            l=inorder(root.left,l)
+            l.append(root.val)
+            l=inorder(root.right,l)
+            return l
+        def postorder(root,l):
+            if not root:
+                l.append(None)
+                return l
+            l=postorder(root.left,l)
+            l=postorder(root.right,l)
+            l.append(root.val)
+            return l
+        return postorder(p,[])==postorder(q,[]) and preorder(p,[])==preorder(q,[]) and inorder(p,[])==inorder(q,[])
