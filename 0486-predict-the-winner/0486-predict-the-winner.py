05f05f46c71abb927cb05249750b6ae6b273a798
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-07-31 23:30:21 +0300

    Time: 52 ms (31.07%), Space: 16.5 MB (31.66%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -0,0 +1,8 @@
+class Solution:
+    def PredictTheWinner(self, nums: List[int]) -> bool:
+        @cache
+        def check(start,end):
+            if start == end:
+                return nums[start]
+            return max(nums[start] - check(start+1,end),nums[end] -check(start,end-1))
+        return 0<=check(0,len(nums)-1)
