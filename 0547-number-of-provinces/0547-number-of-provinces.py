ada13b4a0178c4f995db535de35219f74a1add3c
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-08-23 12:55:34 +0300

    Time: 195 ms (98.05%), Space: 17.2 MB (62.82%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n=len(isConnected)
+        count=0
+        stor=[i for i in range(n)]
+        visited=[False]*n
+        while len(stor)>0:
+            count+=1
+            queue=deque()
+            queue.append(stor[0])
+            visited[stor[0]]=True
+            stor.pop(0)
+            while queue:
+                vertex=queue.popleft()
+                for i in range(n):
+                    if isConnected[vertex][i]==1 and not visited[i]:
+                        queue.append(i)
+                        visited[i]=True
+                        stor.remove(i)
+        return count
+        
