58d27ab45d3268f3b48d2ab34b72f52a87b2b691
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-08-25 15:20:17 +0300

    Time: 320 ms (56.76%), Space: 24.1 MB (39.66%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -12,7 +12,7 @@ class Solution:
             queue=deque()
             queue.append((i,j))
             while queue:
-                row,col=queue.popleft()
+                row,col=queue.pop()
                 for dr,dc in directions4:
                     newr,newc=row+dr,col+dc
                     if valid(newr,newc) and (newr,newc) not in visited and grid[newr][newc]=="1":
