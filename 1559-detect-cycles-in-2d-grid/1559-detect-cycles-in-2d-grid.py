c5111703cec0635fbbee646cabc992edb55fa759
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-08-27 22:12:22 +0300

    Time: 2165 ms (70.03%), Space: 183.7 MB (52.86%) - LeetHub

--- 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.py
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.py
@@ -1,36 +1,23 @@
 class Solution:
     def containsCycle(self, grid: List[List[str]]) -> bool:
-        m, n = len(grid), len(grid[0]) if grid else 0
-        visited = [[False] * n for _ in range(m)]
-        memo = [[None] * n for _ in range(m)]
-
-        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-
-        def dfs(r, c, parent_r, parent_c, char):
-            if memo[r][c] is not None:
-                return memo[r][c]
-
-            visited[r][c] = True
-
-            for dr, dc in directions:
-                row, col = r + dr, c + dc
-
-                if 0 <= row < m and 0 <= col < n and grid[row][col] == char:
-                    if (row, col) == (parent_r, parent_c):
+        directions=[(0,1),(1,0),(-1,0),(0,-1)]
+        def check(row,col):
+            return 0<=row<len(grid) and 0<=col<len(grid[0])
+        def dfs(r,c,pr,pc,val):
+            grid[r][c]=val.upper()
+            for dr,dc in directions:
+                row=dr+r
+                col=dc+c
+                if check(row,col):
+                    if (row,col)==(pr,pc):
                         continue
-
-                    if visited[row][col] or dfs(row, col, r, c, char):
-                        memo[r][c] = True
+                    if grid[row][col]==val.upper():
                         return True
-
-            visited[r][c] = False
-            memo[r][c] = False
+                    if grid[row][col]==val and dfs(row,col,r,c,val):
+                        return True
             return False
-
-        for r in range(m):
-            for c in range(n):
-                if not visited[r][c]:
-                    if dfs(r, c, -1, -1, grid[r][c]):
-                        return True
-
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j]>"Z" and  dfs(i,j,-1,-1,grid[i][j]):
+                    return True
         return False
