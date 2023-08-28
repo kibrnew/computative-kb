a6f784fbabb38e4501c43d6f7d902022b8d4fc79
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-08-28 22:44:53 +0300

    Time: 191 ms (50.13%), Space: 19.2 MB (51.84%) - LeetHub

--- 0529-minesweeper/0529-minesweeper.py
+++ 0529-minesweeper/0529-minesweeper.py
@@ -2,26 +2,28 @@ class Solution:
     def updateBoard(self, board: List[List[str]], click: List[int]) -> List[List[str]]:
         def check(row,col):
             return 0 <= row < len(board) and 0 <= col < len(board[0])
+        def count(row, col):
+            count = 0
+            for dr, dc in directions:
+                r = row + dr
+                c = col + dc
+                if check(r,c) and board[r][c] == 'M':
+                    count += 1
+            return count
         def dfs(row, col):
-    
             if board[row][col] == 'E':
                 adjacent = count(row, col)
                 if adjacent == 0:
                     board[row][col] = 'B'
                     for dr, dc in directions:
-                        r, c = row + dr, col + dc
+                        r = row + dr
+                        c = col + dc
                         if check(r,c):
                             dfs(r, c)
                 else:
                     board[row][col] = str(adjacent)
 
-        def count(row, col):
-            count = 0
-            for dr, dc in directions:
-                r, c = row + dr, col + dc
-                if check(r,c) and board[r][c] == 'M':
-                    count += 1
-            return count
+      
 
         directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
         row,col = click
