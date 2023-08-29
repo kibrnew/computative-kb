d265e0d959c67f30034d1752805e5d24f38b97a9
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-08-29 23:52:03 +0300

    Time: 86 ms (86.56%), Space: 24.3 MB (26.08%) - LeetHub

--- 0609-find-duplicate-file-in-system/0609-find-duplicate-file-in-system.py
+++ 0609-find-duplicate-file-in-system/0609-find-duplicate-file-in-system.py
@@ -1,18 +1,19 @@
+from collections import defaultdict
 
 class Solution:
     def findDuplicate(self, paths):
-        content_to_paths = defaultdict(list)
+        topaths = defaultdict(list)
 
         for path in paths:
             parts = path.split()
             directory = parts[0]
-            for file_info in parts[1:]:
-                file_parts = file_info.split('(')
-                file_name = file_parts[0]
-                content = file_parts[1][:-1]  # Remove the closing parenthesis ')'
-                full_path = f"{directory}/{file_name}"
-                content_to_paths[content].append(full_path)
+            for finfo in parts[1:]:
+                fparts = finfo.split('(')
+                fname = fparts[0]
+                content = fparts[1][:-1]
+                fullpath = f"{directory}/{fname}"
+                topaths[content].append(fullpath)
 
-        duplicate_groups = [group for group in content_to_paths.values() if len(group) > 1]
+        groups = [group for group in topaths.values() if len(group) > 1]
 
-        return duplicate_groups
+        return groups
