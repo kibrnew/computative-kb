2bd4afa10aa3cb629b344c007a56e3eee3ae8101
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-08-30 22:10:46 +0300

    Time: 54 ms (22.44%), Space: 16.6 MB (10.03%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,16 +1,19 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         temp=[]
+        visited=set()
         ans=[]
         def track():
             for num in nums:
                 if len(nums)==len(temp):
                     ans.append(temp[:])
                     return 
-                if num not in temp:
+                if num not in visited:
                     temp.append(num)
+                    visited.add(num)
                     track()
                     temp.pop()
+                    visited.remove(num)
         track()            
         return ans   
             
