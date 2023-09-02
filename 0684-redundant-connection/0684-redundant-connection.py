b796b9232b62b4c83c00b4322de1c02821ed5787
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-09-02 15:33:06 +0300

    Time: 69 ms (40.16%), Space: 17.8 MB (5.73%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -1,24 +1,17 @@
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        def find(parent, x):
-            if parent[x] != x:
-                parent[x] = find(parent, parent[x])
-            return parent[x]
-
-        def union(parent, x, y):
-            root_x = find(parent, x)
-            root_y = find(parent, y)
-            if root_x != root_y:
-                parent[root_x] = root_y
-
-        n = len(edges)
-        parent = list(range(n + 1))  # Create a parent list to represent disjoint sets
-
-        for edge in edges:
-            u, v = edge
-            if find(parent, u) == find(parent, v):
-                return edge
-            else:
-                union(parent, u, v)
-
-        return []  # Return an empty list if no redundant edge is found
+        graph=defaultdict(set)
+        def dfs(node,target):
+            if node not in visited:
+                visited.add(node)
+                if node==target:
+                    return True 
+                for child in graph[node]:
+                    if dfs(child,target):
+                        return True 
+        for root,edge in edges:
+            visited=set()
+            if root in graph and edge in graph and dfs(root,edge):
+                return root,edge
+            graph[root].add(edge)
+            graph[edge].add(root)
