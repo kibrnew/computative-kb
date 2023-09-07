7b739dbef07116d7a14586a7a753d3a7433a0875
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-09-07 15:40:30 +0300

    Time: 300 ms (89.56%), Space: 19.1 MB (78.30%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.py
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.py
@@ -1,26 +1,16 @@
-class Solution:
-    def findLength(self, nums1a: List[int], nums2b: List[int]) -> int:  
-        def check(nums1,nums2):
-            n=len(nums1)
-            m=len(nums2)
-            ans=0
-            for i,first in enumerate(nums1):
-                left=0
-                w=0
-                while left<m:
-                    w=0
-                    if first==nums2[left]:
-                        while left+w<m and i+w<n and nums1[i+w]==nums2[left+w]:
-                            w+=1
-                        ans=max(ans,w)
-                        if nums1[i+w-1]==nums2[left+w-1]:
-                            w-=1
-                        left+=w
-                    left+=1
-            return ans 
-        return max(check(nums1a,nums2b),check(nums2b,nums1a))
+class Solution(object):
+    def findLength(self, A, B):
+        def check(length):
+            seen = set(tuple(A[i:i+length]) 
+                       for i in range(len(A) - length + 1))
+            return any(tuple(B[j:j+length]) in seen 
+                       for j in range(len(B) - length + 1))
 
-                
-            
-        
-        
+        lo, hi = 0, min(len(A), len(B)) + 1
+        while lo < hi:
+            mi = (lo + hi) // 2
+            if check(mi):
+                lo = mi + 1
+            else:
+                hi = mi
+        return lo - 1
