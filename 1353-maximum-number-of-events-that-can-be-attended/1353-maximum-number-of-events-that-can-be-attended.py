70c6654f13019522f308e9e58bcdb0c18a66a111
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-09-08 11:45:26 +0300

    Time: 897 ms (95.17%), Space: 51.4 MB (91.40%) - LeetHub

--- 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.py
+++ 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.py
@@ -1,15 +1,16 @@
 class Solution:
     def maxEvents(self, events: List[List[int]]) -> int:
         events.sort(reverse=True)
-        temp= []
-        ans= d = 0
-        while events or temp:
-            if not temp: d = events[-1][0]
-            while events and events[-1][0] <= d:
-                heapq.heappush(temp, events.pop()[1])
-            heapq.heappop(temp)
+        heap= []
+        ans= cur = 0
+        while events or heap:
+            if not heap:
+                cur= events[-1][0]
+            while events and events[-1][0] <= cur:
+                heappush(heap, events.pop()[1])
+            heappop(heap)
             ans+= 1
-            d += 1
-            while temp and temp[0] < d:
-                heapq.heappop(temp)
+            cur += 1
+            while heap and heap[0] < cur:
+                heappop(heap)
         return ans
