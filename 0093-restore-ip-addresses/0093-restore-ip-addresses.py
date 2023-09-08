01bc1b579ee5325fe3e5d70e3f4638754aef07ef
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-09-08 16:08:22 +0300

    Time: 46 ms (52.93%), Space: 16.4 MB (22.10%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.py
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -6,17 +6,14 @@ class Solution:
                 if len(remain)==0:
                     ans.append(".".join(cur))
                 return
-            maxum=9-3*len(cur)
-            minum=3-len(cur)
-            i=1
-            while minum<=len(remain)-i:
-                if maxum>=len(remain)-i:
-                    new=cur+[remain[:i]]
-                    val=int(remain[:i])
-                    # print(i,val)
-                    if val<256 and (i==1 or val>=10**(i-1)):
-                        dfs(new,remain[i:])
-                i+=1
+            elif len(remain)==0:
+                return 
+            for i in range(1,4):
+                new=cur+[remain[:i]]
+                val=int(remain[:i])
+                # print(i,val)
+                if val<256 and (i==1 or val>=10**(i-1)):
+                    dfs(new,remain[i:])
         if len(s)>12:
             return []
         dfs([],s)
