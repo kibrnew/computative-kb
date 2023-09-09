4cb55751cec8baeed566aa0d1c6a0ee6e5f4eb05
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-09-09 23:46:01 +0300

    Time: 102 ms (55.30%), Space: 19.8 MB (88.40%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -5,14 +5,41 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        temp=[]
-        for node in lists:
-            head=node
-            while head:
-                temp.append(head.val)
-                head=head.next
-        temp.sort(reverse=True)
-        ans=None 
-        for i in temp:
-            ans=ListNode(i,ans)
-        return ans
+        # if len(lists)<1:
+        #     return None
+        def merge(h1,h2):
+            cur=ListNode(-float("inf"))
+            ans=cur
+            while h1 and h2:
+                if h2.val>=h1.val:
+                    cur.next=h1
+                    cur=cur.next
+                    h1=h1.next
+                else:
+                    cur.next=h2
+                    cur=cur.next
+                    h2=h2.next
+            if not h1:
+                cur.next=h2
+            else:
+                cur.next=h1
+            return ans.next
+        
+        # left,right=lists
+        
+        def merge_sort(l):
+            if len(l)<2:
+                if not l:
+                    return None
+                return l[0]
+            else:
+                middle=len(l)//2
+                left=merge_sort(l[:middle])
+                right=merge_sort(l[middle:])
+                return merge(left,right)
+        
+        
+        return merge_sort(lists)
+        
+                
+                
