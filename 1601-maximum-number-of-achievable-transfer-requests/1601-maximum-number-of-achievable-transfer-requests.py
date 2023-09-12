defca892b10c2eba45cca99222a3a9aab35ebf19
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-09-12 21:33:03 +0300

    Time: 1836 ms (25.01%), Space: 16.5 MB (28.09%) - LeetHub

---
+++ 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maximumRequests(self, n: int, requests: List[List[int]]) -> int:
+        l=len(requests)
+        ans=0
+        temp=[0]*n
+        
+        def track(ind,count):
+            print(ind)
+            if ind==l:
+                if all (x==0 for x in temp):
+                    nonlocal ans
+                    ans=max(ans,count)
+                return 
+            track (ind+1,count)
+            
+            start,end=requests[ind]
+            temp[start]-=1
+            temp[end]+=1
+            track(ind+1,count+1)
+            temp[end]-=1
+            temp[start]+=1
+            
+        track(0,0)
+        return ans 
+        
