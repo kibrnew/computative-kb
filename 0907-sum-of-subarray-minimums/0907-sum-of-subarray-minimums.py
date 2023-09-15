5f62965107daa63494cf32fe00ab23f10220b436
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-09-15 22:52:51 +0300

    Time: 354 ms (99.13%), Space: 20.8 MB (90.64%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
@@ -1,38 +1,16 @@
 class Solution:
     def sumSubarrayMins(self, arr: List[int]) -> int:
-        MOD = 10**9 + 7
-        stack = []  # Stack to store the indices of elements in ascending order
-        
-        # Arrays to store left and right boundaries for each element
-        left_boundaries = [0] * len(arr)
-        right_boundaries = [0] * len(arr)
-        
-        # Find the left boundaries for each element
-        for i in range(len(arr)):
-            while stack and arr[i] < arr[stack[-1]]:
-                stack.pop()
-            if stack:
-                left_boundaries[i] = stack[-1] + 1
-            else:
-                left_boundaries[i] = 0
-            stack.append(i)
-        
-        stack = []  # Reset the stack
-        
-        # Find the right boundaries for each element
-        for i in range(len(arr) - 1, -1, -1):
-            while stack and arr[i] <= arr[stack[-1]]:
-                stack.pop()
-            if stack:
-                right_boundaries[i] = stack[-1] - 1
-            else:
-                right_boundaries[i] = len(arr) - 1
-            stack.append(i)
-        
-        # Calculate the sum of minimums for all subarrays
-        result = 0
-        for i in range(len(arr)):
-            result += arr[i] * (i - left_boundaries[i] + 1) * (right_boundaries[i] - i + 1)
-            result %= MOD
-        
-        return result
+        ans, stack = 0, []
+        for num in arr:
+            cnt = 1
+            while stack and stack[-1][0] >= num:
+                n, c = stack.pop()
+                ans += n*c*cnt
+                cnt += c
+            stack.append((num, cnt))
+        cnt = 1
+        while stack:
+            n, c = stack.pop()
+            ans += n * c * cnt
+            cnt += c
+        return ans % (10**9 + 7)
