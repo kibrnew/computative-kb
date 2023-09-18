1f929791431bce295a6ff52dc1425ebf605bb0a8
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-09-18 22:09:11 +0300

    Time: 437 ms (27.87%), Space: 20.7 MB (98.99%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.py
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.py
@@ -2,28 +2,21 @@ class Solution:
     def maxSubarraySumCircular(self, nums: List[int]) -> int:
         n = len(nums)
 
-        # Helper function to find maximum subarray sum
-        def max_subarray_sum(nums):
-            max_sum = float('-inf')
-            current_sum = 0
+        def maxSum(nums):
+            maxSum = float('-inf')
+            currSum = 0
             for num in nums:
-                current_sum = max(num, current_sum + num)
-                max_sum = max(max_sum, current_sum)
-            return max_sum
+                currSum = max(num, currSum + num)
+                maxSum = max(maxSum, currSum)
+            return maxSum
 
-        # Case 1: Maximum subarray sum without circular
-        max_without_circular = max_subarray_sum(nums)
+        maxWithoutCircular = maxSum(nums)
 
-        # Case 2: Maximum subarray sum with circular
-        # We can find this by calculating the maximum subarray sum of the inverted array
-        # and subtracting it from the total sum of the original array.
-        total_sum = sum(nums)
-        inverted_nums = [-num for num in nums]
-        max_with_circular = total_sum + max_subarray_sum(inverted_nums)
+        totalSum = sum(nums)
+        invertedNums = [-num for num in nums]
+        maxWithCircular = totalSum + maxSum(invertedNums)
 
-        # If all elements are negative, return the maximum subarray sum without circular
-        if max_with_circular == 0:
-            return max_without_circular
+        if maxWithCircular == 0:
+            return maxWithoutCircular
 
-        # Return the maximum of the two cases
-        return max(max_without_circular, max_with_circular)
+        return max(maxWithoutCircular, maxWithCircular)
