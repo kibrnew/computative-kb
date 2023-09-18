0fab6b4e89b5b1d92ca1f9bcd0be590a11f3c51e
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-09-18 22:05:15 +0300

    Time: 410 ms (65.75%), Space: 20.7 MB (98.99%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.py
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.py
@@ -1,14 +1,29 @@
 class Solution:
     def maxSubarraySumCircular(self, nums: List[int]) -> int:
-        cur_max = 0
-        cur_min = 0
-        _sum = 0
-        max_sum = nums[0]
-        min_sum = nums[0]
-        for num in nums:
-            cur_max = max(cur_max, 0) + num
-            max_sum = max(max_sum, cur_max)
-            cur_min = min(cur_min, 0) + num
-            min_sum = min(min_sum, cur_min)
-            _sum += num
-        return max_sum if _sum == min_sum else max(max_sum, _sum - min_sum)
+        n = len(nums)
+
+        # Helper function to find maximum subarray sum
+        def max_subarray_sum(nums):
+            max_sum = float('-inf')
+            current_sum = 0
+            for num in nums:
+                current_sum = max(num, current_sum + num)
+                max_sum = max(max_sum, current_sum)
+            return max_sum
+
+        # Case 1: Maximum subarray sum without circular
+        max_without_circular = max_subarray_sum(nums)
+
+        # Case 2: Maximum subarray sum with circular
+        # We can find this by calculating the maximum subarray sum of the inverted array
+        # and subtracting it from the total sum of the original array.
+        total_sum = sum(nums)
+        inverted_nums = [-num for num in nums]
+        max_with_circular = total_sum + max_subarray_sum(inverted_nums)
+
+        # If all elements are negative, return the maximum subarray sum without circular
+        if max_with_circular == 0:
+            return max_without_circular
+
+        # Return the maximum of the two cases
+        return max(max_without_circular, max_with_circular)
