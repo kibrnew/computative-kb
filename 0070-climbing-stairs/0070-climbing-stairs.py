11560ee2048a707a539af46b403c2a86835c9fce
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-09-20 23:44:11 +0300

    Time: 38 ms (55.62%), Space: 16.3 MB (23.99%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,14 +1,10 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         
-        # @cache
-        # def fib(k):
-        #     if k<=1:
-        #         return 1
-        #     else :
-        #         return fib(k-1)+fib(k-2)
-        pr1=1
-        pr2=1
-        for i in range(n):
-            pr1,pr2=pr1+pr2,pr1
-        return pr2
+        @cache
+        def fib(k):
+            if k<=1:
+                return 1
+            else :
+                return fib(k-1)+fib(k-2)
+        return fib(n)
