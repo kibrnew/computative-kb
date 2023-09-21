b8cc39062a412bc57e9fa264f137a28f66c52fc2
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-09-22 00:04:46 +0300

    Time: 41 ms (48.36%), Space: 16.4 MB (32.48%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,17 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n=len(nums)
+        if n==1:
+            return nums[0]
+        dp={}
+        def recur(k):
+            if k in dp :
+                return dp[k]
+            if k<2:
+                if k==1:
+                    return max(nums[0],nums[1])
+                return nums[0]
+            dp[k]=max(nums[k]+recur(k-2),recur(k-1))
+            return dp[k]
+        return recur(n-1)
+        
