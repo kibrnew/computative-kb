c119221182134997bcc316d2db8837cb51c2206e
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-09-22 18:00:07 +0300

    Time: 138 ms (7.52%), Space: 19.4 MB (92.91%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,9 +2,16 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
             stor=defaultdict(list)
             n=len(strs)
+            def count(val):
+                t=Counter(val)
+                temp=[]
+                k=sorted(t.keys())
+                for key in k:
+                    temp.append(key+str(t[key]))
+                return "".join(temp)
             for word in strs:
-                temp=sorted(word)
-                stor["".join(temp)].append(word)
+                temp=count(word)
+                stor[temp].append(word)
             return list(stor.values())
             
                 
