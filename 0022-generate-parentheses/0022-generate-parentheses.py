d6b6ae7c14482909cee954753fadd1c6b7b49ac9
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-09-22 16:45:15 +0300

    Time: 28 ms (99.05%), Space: 16.7 MB (55.49%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,15 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        ans=[]
+        
+        def dfs(cur, opened, closed):
+            if len(cur) == 2 * n:
+                ans.append(cur)
+                return
+            if opened< n:
+                dfs(cur + '(', opened + 1, closed)
+            if closed < opened:
+                dfs(cur + ')', opened, closed + 1)
+                
+        dfs("", 0, 0)
+        return ans
