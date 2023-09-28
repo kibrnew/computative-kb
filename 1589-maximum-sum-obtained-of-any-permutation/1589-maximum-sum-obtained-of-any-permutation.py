d5951bf2bbb547b43dbd0c6cbcbfc9072b986e73
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-09-28 20:08:28 +0300

    Time: 1426 ms (53.70%), Space: 50.5 MB (29.63%) - LeetHub

--- 1589-maximum-sum-obtained-of-any-permutation/1589-maximum-sum-obtained-of-any-permutation.py
+++ 1589-maximum-sum-obtained-of-any-permutation/1589-maximum-sum-obtained-of-any-permutation.py
@@ -1,6 +1,7 @@
 class Solution:
     def maxSumRangeQuery(self, nums: List[int], requests: List[List[int]]) -> int:
             n = len(nums)
+            nums.sort()
             count = [0] * (n + 1)
             for i, j in requests:
                 count[i] += 1
@@ -8,7 +9,9 @@ class Solution:
             for i in range(1, n + 1):
                 count[i] += count[i - 1]
             ans = 0
-            for v, c in zip(sorted(count[:-1]), sorted(nums)):
-                ans += v * c
+            count.pop()
+            count.sort()
+            for i in range(len(nums)):
+                ans += nums[i]*count[i]
             return ans % (10**9 + 7)
         
