5af807842fff7e874648a218db982884ce9ce144
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-10-04 22:47:47 +0300

    Time: 46 ms (26.12%), Space: 16.3 MB (20.49%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,12 +1,10 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        pr1=list(s)
-        pr2=list(t)
+        count=Counter(s)
         
-        pr1.sort()
-        pr2.sort()
-        for i in range(len(pr1)):
-            if not pr1[i]==pr2[i]:
-                return pr2[i]
-        return pr2[-1]
+        for a in t :
+            if count[a]==0:
+                return a
+            else:
+                count[a]-=1
         
