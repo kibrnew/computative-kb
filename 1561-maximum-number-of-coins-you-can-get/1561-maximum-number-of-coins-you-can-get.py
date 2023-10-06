2809cd122e78cbab21cd5cb6a548db88d8664f30
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-10-06 13:26:04 +0300

    Time: 535 ms (34.88%), Space: 28.6 MB (85.95%) - LeetHub

--- 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.py
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.py
@@ -3,6 +3,6 @@ class Solution:
         piles.sort()
         ans=0
         for i in range(len(piles)//3):
-            print(piles[-2*i-2])
+            # print(piles[-2*i-2])
             ans += piles[-2*i-2]
         return ans
