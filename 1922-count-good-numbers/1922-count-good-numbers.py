e53277e1d9843d8b3b89fa243b62263d09d12132
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-10-12 14:57:11 +0300

    Time: 37 ms (70.19%), Space: 16.2 MB (82.16%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.py
+++ 1922-count-good-numbers/1922-count-good-numbers.py
@@ -1,15 +1,21 @@
 class Solution:
     def countGoodNumbers(self, n: int) -> int:
-        def power(k,n,m):
-            if n==0:
-                return 1
-            if n==1:
-                return k%m
-            if n%2==1:
-                return (power(k,n//2,m)**2*(k%m))%m
-            else:
-                return power(k,n//2,m)**2%m
-        ans= power(20, n//2, 10**9 + 7)
-        if n%2==1: 
-            ans*=5
-        return ans%(10**9+7)
+        
+        def power(a,b,m):
+            result=1
+            
+            while b>0:
+                if b&1:
+                    result=((result%m)*(a%m))%m
+                a=(a*a)%m
+                b>>=1
+            return result
+        
+        evens=5
+        odds=4 
+        
+        return (power(evens,(n+1)//2,10**9 + 7)*power(odds,n//2,10**9 + 7))%(10**9 + 7)
+        
+        
+            
+            
