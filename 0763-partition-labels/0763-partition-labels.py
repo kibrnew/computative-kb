69ee00a1f97a4ed96036c748d0062dc19da93901
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-10-13 18:07:36 +0300

    Time: 48 ms (41.07%), Space: 16.1 MB (97.64%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.py
+++ 0763-partition-labels/0763-partition-labels.py
@@ -1,37 +1,23 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        w=[]
+        cur=0
+        ind=s.rfind(s[0])
         ans=[]
-        size=len(s)
-        pr1=0
-        for i in range(size):
-            w.append(s.rfind(s[i]))
-        pr2=w[0]
-        while pr2<size:
-            if pr1==pr2:
-                ans.append(pr1)
-                if (pr2+1)==size:
-                    break
-                pr1+=1
-                pr2=w[pr1]
-            while pr1<pr2:
-                if w[pr1]>pr2:
-                    pr2=w[pr1]
-                    pr1=0
-                    break
-                pr1+=1
-                #if pr1+1 == pr2:
-                  #  ans.appen(pr1)
+        last=-1
+        while ind<len(s):
+            ind=max(ind,s.rfind(s[cur]))
+            while cur<ind:
+                ind=max(ind,s.rfind(s[cur]))
+                cur+=1
+            ans.append(ind-last)
+            last=ind
+            ind+=1
+            cur=ind
+        return ans
+    
+            
                 
-                
-        ans2=[]
-        for i in range(len(ans)):
-            if i==0:
-                ans2.append(ans[0]+1)
-            else:
-                ans2.append(ans[i]-ans[i-1])
-                
-        return ans2
+            
                 
             
             
