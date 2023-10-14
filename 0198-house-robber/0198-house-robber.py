54724a5b152542552d1355e67d107b0ebd02bd28
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-10-14 10:19:43 +0300

    Time: 47 ms (8.56%), Space: 16.4 MB (5.11%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -7,9 +7,9 @@ class Solution:
         def recur(k):
             if k in dp :
                 return dp[k]
-            if k<2:
-                if k==1:
-                    return max(nums[0],nums[1])
+            if k==1:
+                return max(nums[0],nums[1])
+            if k==0:
                 return nums[0]
             dp[k]=max(nums[k]+recur(k-2),recur(k-1))
             return dp[k]
