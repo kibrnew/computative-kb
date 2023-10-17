9c7b7a857c472a11f4cdfe3edf7a83d7809e5415
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-10-17 10:30:34 +0300

    Time: 2178 ms (12.11%), Space: 389.7 MB (31.65%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.py
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.py
@@ -1,13 +1,18 @@
 class Solution:
     def maxAlternatingSum(self, nums: List[int]) -> int:
         
-        @cache
+        cache = {}
         def dfs(i,op):
             if i==len(nums):
-                    return 0
+                return 0
+            if (i, op) in cache:
+                return cache[(i, op)] 
+            
             if op=="+":
-                return max(dfs(i+1,"-")+nums[i],dfs(i+1,"+"))
+                cache[(i, op)] = max(dfs(i+1,"-")+nums[i],dfs(i+1,"+"))
+                return cache[(i, op)]
             else:
-                return max(dfs(i+1,"+")-nums[i],dfs(i+1,"-"))
+                cache[(i, op)]=max(dfs(i+1,"+")-nums[i],dfs(i+1,"-"))
+                return cache[(i, op)]
                 
         return dfs(0,"+")
