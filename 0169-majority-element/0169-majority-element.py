0a62f98aabda76f751df3cd3be245f79666b9ddc
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-11-02 23:42:04 +0300

    Time: 144 ms (85.15%), Space: 17.8 MB (37.75%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,10 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        
+        count=Counter(nums)
+        n=len(nums)//2
+        for key,value in count.items():
+            if value>n:
+                return key
+        
+        
