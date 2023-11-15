9cd4e36cf92c434a3ee1e653e41a5f7a11bbcc43
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-11-15 18:21:25 +0300

    Time: 704 ms (97.12%), Space: 38.6 MB (47.01%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -1,19 +1,17 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        time=defaultdict(int)
-        n=len(position)
-        for i in range(n):
-            time[position[i]]=(target-position[i])/speed[i]
-        position.sort()
-        ans=[time[position[0]]]
-
-        for i in range(1,n):
-            temp=time[position[i]]
-            if temp<ans[-1]:
-                ans.append(temp)
-            else:
-                while ans and ans[-1]<=temp:
-                    ans.pop()
-                ans.append(temp)
-        return len(ans)
         
+        temp=sorted(zip(position,speed),reverse=True)
+        maxi=0
+        ans=0
+        # print(temp)
+        for pos,speed in temp:
+            t=(target-pos)/speed
+            if t>maxi:
+                ans+=1
+                maxi=t
+        return ans
+                
+            
+            
+      
