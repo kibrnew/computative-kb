8831a5bb6016bb35abefb78d31e584a69592e8ee
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-12-05 20:04:41 +0300

    Time: 56 ms (24.65%), Space: 17.4 MB (43.66%) - LeetHub

--- 1184-distance-between-bus-stops/1184-distance-between-bus-stops.py
+++ 1184-distance-between-bus-stops/1184-distance-between-bus-stops.py
@@ -1,10 +1,21 @@
 class Solution:
     def distanceBetweenBusStops(self, distance: List[int], start: int, destination: int) -> int:    
         n=len(distance)
-        distance=distance+distance
+        # distance=distance+distance
         first=min(start,destination)
         last=max(start,destination)
         
-        return min(sum(distance[last:first+n]),sum(distance[first:last]))
+        ans1=0
+        ans2=0
+        for i in range(first,n+first):
+            ind=i%n
+            if i<last:
+                ans1+=distance[i]
+            elif i<start+n:
+                ans2+=distance[ind]
+        return min(ans1,ans2)
+                
+        
+        # return min(sum(distance[last:first+n]),sum(distance[first:last]))
         
         
