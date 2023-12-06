0ca5be4b239126c90afcd97f7bb5d8dedc72def9
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-12-06 19:38:22 +0300

    Time: 34 ms (83.17%), Space: 16.3 MB (60.99%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -11,36 +11,47 @@ class Solution:
         ans=[]
         
         for _ in range(min(len(matrix),len(matrix[0]))):
+            
             if direction:
+                
+                # if direction is True our sprial goes right then down 
+                
+                # right
                 j=j_start
                 while j<=j_end:
                     ans.append(matrix[i_start][j])
                     j+=1
-                i_start+=1
+                    
+                i_start+=1     #  not to print elements from visited upper rows 
                 
+                # down
                 i=i_start
                 while i<=i_end:
                     ans.append(matrix[i][j_end])
                     i+=1
-                j_end-=1
+                    
+                j_end-=1     # not to print elements from visited right columns 
                 
-
             else:
+                # if direction is False our spiral goes left then up 
+                
+                # left
                 j=j_end
                 while j>=j_start:
                     ans.append(matrix[i_end][j])
                     j-=1
-                i_end-=1
+                    
+                i_end-=1         #  not to print elements from visited the bottom rows 
                 
+                #up
                 i=i_end
                 while i>=i_start:
                     ans.append(matrix[i][j_start])
                     i-=1
-                j_start+=1
+                    
+                j_start+=1       # not to print elements from visited left columns
 
-
-        
-            direction=not(direction)
+            direction=not(direction) 
             
                 
         return ans
