f77db38b8ba9a42b28014741a5069dca795fba68
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-12-12 17:17:54 +0300

    Added solution for Two Sum

---
+++ group5/two-sum.py
@@ -0,0 +1,17 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        rev={}
+        
+        for i,val in enumerate(nums):
+            
+            diff=target-val
+            
+            if diff in rev:
+                return [i,rev[diff]]
+            
+            rev[val]=i
+            
+       
+        
+        
