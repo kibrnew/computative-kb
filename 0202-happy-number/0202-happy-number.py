815ed7b8ff7914acb84f4feaa556cbb2a08011f8
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-12-12 17:54:25 +0300

    Time: 36 ms (88.39%), Space: 16.1 MB (96.05%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,10 +1,16 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
+        visited=set()
         
         def ssum(m):
             
             return sum([int(val)**2 for val in str(m)])
         
-        for _ in range(7):
+        while n!=1:
+            
             n=ssum(n)
-        return n==1
+            if n in visited:
+                return False
+            visited.add(n)
+            
+        return True
