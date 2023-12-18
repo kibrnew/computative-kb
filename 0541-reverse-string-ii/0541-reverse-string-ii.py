cb4fa4cbcf29048a39b8988c56ba820837050fc9
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-12-18 23:07:54 +0300

    Time: 41 ms (55.61%), Space: 16.4 MB (53.54%) - LeetHub

--- 0541-reverse-string-ii/0541-reverse-string-ii.py
+++ 0541-reverse-string-ii/0541-reverse-string-ii.py
@@ -1,18 +1,15 @@
 class Solution:
     def reverseStr(self, s: str, k: int) -> str:
         
-        ans=list(s)
+        ans=[]
         n=len(s)
-        ans[:k]=ans[k-1::-1]
-        for i in range(2*k,n-2*k,2*k):
-            ans[i:i+k]=ans[i+k-1:i-1:-1]
-            # print(ans[i:i+k],)
+        flag=True
+        # print(s[:15])
         
-        i=n%(2*k)
-        # print(i,ans)
-        if n>2*k:
-            if i>k:
-                ans[-i:-i+k]=ans[-1-i+k:-i-1:-1]
+        for i in range(0,n,k):
+            if flag:
+                ans.append(s[i:i+k][::-1])
             else:
-                ans[-i:]=ans[:-1-i:-1]
+                ans.append(s[i:i+k])
+            flag= not(flag)
         return "".join(ans)
