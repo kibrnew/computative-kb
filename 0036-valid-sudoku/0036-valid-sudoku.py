256de961362f12c98d8a5e8aa464aaec1d52ccd7
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-12-22 17:57:18 +0300

    Time: 86 ms (99.32%), Space: 17.4 MB (10.01%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,41 +1,22 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         
-        n=len(board)
-        m=len(board[0])
+        row=defaultdict(set)
+        col=defaultdict(set)
+        box=defaultdict(set)
         
-        for i in range(n):
-            count=defaultdict(int)
-            rcount=defaultdict(int)
-            for j in range(m):
+        for i in range(9):
+            for j in range(9):
+                val=board[i][j]
                 
-                if board[i][j]!= ".":
-                    count[board[i][j]]+=1
-                    
-                if board[j][i]!=".":
-                    rcount[board[j][i]]+=1
-            c=list(count.values())+list(rcount.values())  
-            if c:
-                maxi=max(c)
-        
-                if maxi>1:
-                    return False
-        
-        for i in range(0,n,3):
-            
-            for j in range(0,n,3):
-                temp=[]
-                temp.extend(board[i][j:j+3])
-                temp.extend(board[i+1][j:j+3])
-                temp.extend(board[i+2][j:j+3])
-                count=Counter(temp)
-                count.pop(".")
-                if count:
-                    maxi=max(count.values())
-                    if maxi >1:
+                if val!=".":
+                    k=(i//3)*10+j//3
+                    if val in row[i] or val in col[j] or val in box[k]:
                         return False
-        return True 
-                
+                    row[i].add(val)
+                    col[j].add(val)
+                    box[k].add(val)
+        return True
                 
                 
                 
