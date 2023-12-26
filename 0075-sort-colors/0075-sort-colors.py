ff70face47749c2a14291cc2e772f022e7dcefed
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-12-26 19:51:01 +0300

    Time: 40 ms (62.02%), Space: 17.3 MB (5.95%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,19 +3,19 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        one=0
-        zero=0
-        two=0
-        for i in range(len(nums)):
-            if nums[i]==0:
-                zero=zero+1
-            elif nums[i]==1:
-                one=one+1
-            elif nums[i]==2:
-                two=two+1
-        ans= ([0]*zero)+([1]*one)+([2]*two)
-        for i in range(len(nums)):
-            nums[i]=ans[i]
+        
+        ans=[0]*3
+        for val in nums:
+            ans[val]+=1
+            
+        new=[ ]
+        for i in range(3):
+            new.extend([i]*ans[i])
+            
+        nums[::]=new[::]
+        
+        return nums
+            
                 
 
 
