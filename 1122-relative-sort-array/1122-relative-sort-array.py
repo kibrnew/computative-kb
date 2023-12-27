96edf13d886c1ac18fb403c17b3cfcad1cce4ffb
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-12-27 17:25:19 +0300

    Time: 38 ms (89.90%), Space: 17.7 MB (5.61%) - LeetHub

---
+++ 1122-relative-sort-array/1122-relative-sort-array.py
@@ -0,0 +1,38 @@
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+            
+        
+        n=len(arr1)
+        m=len(arr2)
+        check=set(arr2)
+        ind={}
+        for i,val in enumerate(arr2):
+            ind[val]=i
+        
+        def sorter(x):
+            if x in check:
+                return ind[x]
+            else:
+                return (x+2000)
+        arr1.sort(key=sorter)
+        return arr1
+            
+        
+#         ind={}
+#         for i,val in enumerate(arr2):
+#             ind[val]=i
+            
+#         temp=[]
+#         count=[0]*m
+#         for val in arr1:
+#             if val in ind:
+#                 count[ind[val]]+=1
+#             else:
+#                 count.append(val)
+        
+#         ans=[]
+#         for i in range(m):
+#             ans.extend()
+            
+            
+      
