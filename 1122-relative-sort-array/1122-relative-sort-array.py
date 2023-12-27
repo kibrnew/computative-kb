cc61003ee8006fbd6712ad51d4398fdb8881a64e
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-12-27 17:28:55 +0300

    Time: 37 ms (93.09%), Space: 17.4 MB (5.61%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.py
+++ 1122-relative-sort-array/1122-relative-sort-array.py
@@ -13,7 +13,7 @@ class Solution:
             if x in check:
                 return ind[x]
             else:
-                return (x+2000)
+                return (x+1000)
         arr1.sort(key=sorter)
         return arr1
             
