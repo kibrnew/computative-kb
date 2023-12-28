a3235be893de93745e093dd44d602e23895b28e0
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-12-28 13:37:34 +0300

    Time: 163 ms (73.39%), Space: 18.6 MB (6.37%) - LeetHub

--- 0665-non-decreasing-array/0665-non-decreasing-array.py
+++ 0665-non-decreasing-array/0665-non-decreasing-array.py
@@ -3,34 +3,16 @@ class Solution:
         
         count=0
         n=len(nums)
-        ind=n
-        for i in range(n-1,0,-1):
+        
+        for i in range(1,n):
             if nums[i]<nums[i-1]:
                 count+=1
-                ind=i-1
                 
-        if count>=2:
-            return False
-        
-        if count==1:
-            
-            arr1=list(nums)
-            arr2=list(nums)
-            arr1.pop(ind)
-            arr2.pop(ind+1)
-            
-            f1=True
-            f2=True
-            
-            for i in range(n-2):
-                if arr1[i]>arr1[i+1]:
-                    f1=False
-        
-                if arr2[i]>arr2[i+1]:
-                    f2=False
+                if i>1 and nums[i-2]>nums[i]:
+                    nums[i]=nums[i-1]
                     
-            return f1 or f2
-        
-        return True
+        return count<2
+                
+ 
             
        
