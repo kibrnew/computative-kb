658ccf82dbc4b3b157f33175e871ca71febd8ea8
kibrnew <123804824+kibrnew@users.noreply.github.com>
2023-12-29 18:25:36 +0300

    Time: 143 ms (5.08%), Space: 17.2 MB (11.12%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -1,53 +1,17 @@
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        lst = []
-
-        for ele in nums:
-            lst += [str(ele)]
-        print(lst)
-        n = len(lst)
-
+        
+        new=list(map(str,nums))
+        n=len(nums)
         for i in range(n):
-            for j in range(i+1 , n):
+            for j in range(i,n):
                 
-                if str(lst[i]) + str(lst[j]) > str(lst[j]) + str(lst[i]):
-                    # if current order is greatest value .. continue
-                    continue
-                else:
-                    # else swap the values ..!!!
-                    lst[i] , lst[j] = lst[j] , lst[i]
-        
-        
-        ans = ''.join(lst)
-
-        if int(ans) == 0:
+                if int(new[j]+new[i])>int(new[i]+new[j]):
+                    new[i],new[j]=new[j],new[i]
+        ans="".join(new)
+        if int(ans)==0:
             return "0"
-        
         return ans
-
-
         
         
-#         k=[str(i) for i in nums]
-#         k.sort(reverse=True)
-#         ans=[[k[0]]]
-#         i=0
-#         for j in range(1,len(k)):
-#             if k[j][0]==k[j-1][0]:
-#                 ans[i].append(k[j])
-#             else:
-#                 ans.append([k[j]])
-#                 i+=1
-#         def sorter(x):
-#             ad=l-len(x)
-#             n=x+x*((ad//len(x))+1)
-#             mm=n[:l]
-            
-#             return -int(mm)
-#         final=[]
-#         for lis in ans :
-#             m=max(lis,key=lambda x:int(x))
-#             l=len(m)
-#             lis.sort(key=sorter)
-#             final.append("".join(lis))
-#         return "".join(final)
+        
