ec1322e4e97aca99b340b95f76d7324cac7360cf
kibrnew <123804824+kibrnew@users.noreply.github.com>
2024-01-03 17:50:02 +0300

    Time: 50 ms (59.88%), Space: 23.2 MB (5.52%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -2,9 +2,10 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         ans=[]
         for val in s:
-            val=val.lower()
-            if ord("a")<=ord(val)<=ord("z") or ord("0")<=ord(val)<=ord("9"):
+            if val.isalnum():
+                val=val.lower()
                 ans.append(val)
+            
                 
         res="".join(ans)
         return res==res[::-1]
