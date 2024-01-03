375e689fca4b66f129e5b82ffbe0b55c5b91a5fa
kibrnew <123804824+kibrnew@users.noreply.github.com>
2024-01-03 19:20:13 +0300

    Time: 6043 ms (47.66%), Space: 20.9 MB (32.03%) - LeetHub

--- 1163-last-substring-in-lexicographical-order/1163-last-substring-in-lexicographical-order.py
+++ 1163-last-substring-in-lexicographical-order/1163-last-substring-in-lexicographical-order.py
@@ -2,16 +2,13 @@ class Solution:
     def lastSubstring(self, s: str) -> str:
         
         maxi=max(s)
-        ans = []
-        for i in range(len(s)):
-            if s[i]==maxi:
-                ans.append(i)
         sol = ""
-        for n in ans:
-            sol = max(sol, s[n:])
+        first = s.index(maxi)
+        for i in range(first, len(s)):
+            if s[i]==maxi:
+                sol = max(sol, s[i:])
+    
         
-        return sol
-        
-            
+        return sol    
                 
             
