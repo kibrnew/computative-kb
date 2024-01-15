7786f938d7a8e731a073af66e827270783e76a8c
kibrnew <123804824+kibrnew@users.noreply.github.com>
2024-01-15 14:49:48 +0300

    Time: 729 ms (62.15%), Space: 25.5 MB (5.01%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -1,33 +1,19 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        pr1=0
-        count=0
-        f1=0
-        f2=0
-        type2=type1=fruits[0]
-        ans=set([])
-        for i in range(len(fruits)):
-            if fruits[i]==type1 :
-                f1+=1
-                f2=0
-                count+=1
-            elif fruits[i]==type2:
-                f2+=1
-                f1=0
-                count+=1
-            else:
-                if f1==0:
-                    f1=1
-                    count=f2+f1
-                    f2=0
-                    type1=fruits[i]
-                else:
-                    f2=1
-                    count=f2+f1
-                    f1=0
-                    type2=fruits[i]
-            ans.add(count)
-        return max(ans)
+        count=Counter()
+        l=0
+        n=len(fruits)
+        ans=0
+        for r in range(n):
+            count[fruits[r]]+=1
+            while len(count)>2:
+                count[fruits[l]]-=1
+                if count[fruits[l]]==0:
+                    count.pop(fruits[l])
+                l+=1
+            ans=max(ans,r-l+1)
+        return ans
+                
                     
                     
                     
