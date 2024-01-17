01f9796661e29bf60571ad2f7f56351112deccfe
kibrnew <123804824+kibrnew@users.noreply.github.com>
2024-01-17 18:18:15 +0300

    Time: 233 ms (80.20%), Space: 20.3 MB (46.88%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -6,14 +6,17 @@ class Solution:
 #         for i in range(n):
 #             prefix[i+1]=prefix[i]+nums[i]
 
-        count=Counter()
+        count={}
         count[k]=1
         ans=0
         val=0
         for vv in nums:
             val+=vv
             rest=k+val
-            ans+=count[val]
+            ans+=count.get(val,0)
+            if rest not in count:
+                count[rest]=0
+ 
             count[rest]+=1
         # print(count)
         return ans
