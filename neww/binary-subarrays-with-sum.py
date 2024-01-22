2af41f7f640fdd4f58dc52f2df0dcc6d682f3583
kibrnew <123804824+kibrnew@users.noreply.github.com>
2024-01-22 23:39:09 +0300

    Added solution for Binary Subarrays With Sum

---
+++ neww/binary-subarrays-with-sum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        
+        n=len(nums)
+        count=defaultdict(int)
+        s=0
+        ans=0
+        count[0]+=1
+        for i in range(n):
+            s+=nums[i]
+            ans+=count[s-goal]
+            count[s]+=1
+            
+        return ans
