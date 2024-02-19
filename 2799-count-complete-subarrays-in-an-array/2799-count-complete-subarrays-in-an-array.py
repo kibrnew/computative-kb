f060089c7807dffd18352beff2630fee85930a98
kibrnew <123804824+kibrnew@users.noreply.github.com>
2024-02-19 15:12:52 +0300

    Time: 95 ms (66.61%), Space: 17 MB (40.39%) - LeetHub

--- 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.py
+++ 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.py
@@ -1,20 +1,21 @@
 class Solution:
     def countCompleteSubarrays(self, nums: List[int]) -> int:
         
-        k=len(set(nums))-1
+        k=len(set(nums))
         n=len(nums)
         l=0
         count=Counter()
         ans=0
         for r in range(n):
             count[nums[r]]+=1
-            while len(count)>k:
+            while len(count)==k:
+                ans+=n-r
                 count[nums[l]]-=1
                 if count[nums[l]]==0:
                     count.pop(nums[l])
                 l+=1
-            ans+=r-l+1
-        return n*(n+1)//2-ans
+            
+        return ans
             
         
             
