154cc6162f55755ee291aa9982c9c604f9239161
kibrnew <123804824+kibrnew@users.noreply.github.com>
2024-02-20 18:57:02 +0300

    Time: 1046 ms (46.43%), Space: 61.1 MB (60.71%) - LeetHub

---
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minimumEffort(self, tasks: List[List[int]]) -> int:
+        
+        
+        ans=0
+        for a,b in tasks:
+            ans+=a
+        tasks.sort(key=lambda x:x[0]-x[1])
+        
+        res=ans
+        # print(tasks)
+        for a,b in tasks:
+            if res<b:
+                ans+=b-res
+                res=b
+            res-=a
+        return ans 
+       
+        
