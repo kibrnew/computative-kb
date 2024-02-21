565c3412bfcd42a689225956af8d3703ca090fd3
kibrnew <123804824+kibrnew@users.noreply.github.com>
2024-02-21 19:26:23 +0300

    Time: 595 ms (48.96%), Space: 29.3 MB (88.09%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.py
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.py
@@ -1,10 +1,10 @@
 class Solution:
     def minimizeArrayValue(self, nums: List[int]) -> int:
         
-        ans=nums[0]
+        ans=0
         n=len(nums)
-        s=nums[0]
-        for i in range(1,n):
+        s=0
+        for i in range(n):
             s+=nums[i]
             ans=max(ans,(s+i)//(i+1))
         return ans
