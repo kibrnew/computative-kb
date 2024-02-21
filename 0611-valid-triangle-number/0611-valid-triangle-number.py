9ff8bcf1b4e6e89a32e92cd94ab984a080ee72aa
kibrnew <123804824+kibrnew@users.noreply.github.com>
2024-02-21 17:12:17 +0300

    Time: 1734 ms (17.01%), Space: 16.7 MB (52.15%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.py
+++ 0611-valid-triangle-number/0611-valid-triangle-number.py
@@ -4,11 +4,19 @@ class Solution:
         nums.sort()
         ans=0
         n=len(nums)
+        
+        
         for i in range(n-2):
-            if nums[i]==0:
-                continue 
-            for j in range(i+1,n):
-                val=nums[i]+nums[j]
-                ind=bisect_left(nums,val)-1
-                ans+=ind-j
-        return  ans
+            temp=deque()
+            j=i+1
+            while j<=n:
+                while j<n and (len(temp)<1 or nums[j]-temp[0]<nums[i]):
+                    temp.append(nums[j])
+                    j+=1
+                ans+=max(0,len(temp)-1)
+                if len(temp)==0:
+                    j+=1
+                else:
+                    temp.popleft()
+
+        return ans
