f0447b9b33adf3000623fc941fd21ec805b53d71
kibrnew <123804824+kibrnew@users.noreply.github.com>
2024-02-21 15:26:28 +0300

    Add solution for Patching Array

---
+++ leetcode/patching-array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minPatches(self, nums: List[int], n: int) -> int:
+        
+        maxi=0
+        ans=0
+        nums.append(n)
+        for val in nums:
+            if maxi>=n:
+                break
+            while maxi<n and maxi<val-1:
+                maxi=2*maxi+1
+                ans+=1
+            maxi+=val
+        return ans
+                
+        
