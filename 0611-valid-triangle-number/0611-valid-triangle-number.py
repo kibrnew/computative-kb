81b9b4adc4a4732870e5a6aa4f88b35708e2b21c
kibrnew <123804824+kibrnew@users.noreply.github.com>
2024-02-21 16:31:01 +0300

    Time: 1267 ms (25.52%), Space: 16.8 MB (52.15%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.py
+++ 0611-valid-triangle-number/0611-valid-triangle-number.py
@@ -8,7 +8,7 @@ class Solution:
             if nums[i]==0:
                 continue 
             for j in range(i+1,n):
-                val=nums[i]+nums[j]-1
-                ind=bisect_right(nums,val)
-                ans+=ind-j-1
+                val=nums[i]+nums[j]
+                ind=bisect_left(nums,val)-1
+                ans+=ind-j
         return  ans
