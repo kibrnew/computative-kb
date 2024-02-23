df7162ec0c553c1b588e6e1677d5e189aa565bee
kibrnew <123804824+kibrnew@users.noreply.github.com>
2024-02-23 10:13:10 +0300

    Time: 322 ms (80.50%), Space: 21.9 MB (11.16%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
@@ -3,34 +3,16 @@ class Solution:
         
         ans=0
         stack=[-1]
-  
         nums=arr+[-1]
         n=len(nums)
-        right=[0]*(n)
-        left=[0]*(n)
-
+       
         for i in range(n):
             while stack and nums[stack[-1]]>nums[i]:
                 ind=stack.pop()
-                right[ind]=i-ind
-                left[ind]=ind-stack[-1]
-
-            
-        
+                ans+=(i-ind)*(ind-stack[-1])*nums[ind]
+                ans%=(10**9+7)
+             
             stack.append(i)
-
- 
-        left.pop()
-        right.pop()
-        # print(right)
-        # print(left)
-
-        
-        ans=0
-        for i,(a,b) in enumerate(zip(left,right)):
-            ans+=a*b*arr[i]
-            ans%=(10**9+7)
-    
                 
         return ans%(10**9+7)
             
