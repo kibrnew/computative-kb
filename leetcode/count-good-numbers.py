8aab947bf18735c035d069eb8682f4bca2ab4144
kibrnew <123804824+kibrnew@users.noreply.github.com>
2024-02-26 11:48:56 +0300

    Add solution for Count Good Numbers

---
+++ leetcode/count-good-numbers.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countGoodNumbers(self, n: int) -> int:
+
+        even=(n+1)//2
+        odd=n//2
+        return (pow(5,even,10**9 + 7)*pow(4,odd,10**9 + 7))%(10**9 + 7)
+
+            
