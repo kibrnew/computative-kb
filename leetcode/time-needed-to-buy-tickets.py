afc6d8261a7c1a98ade0da06c5aa08d7613ceee4
kibrnew <123804824+kibrnew@users.noreply.github.com>
2024-02-29 14:21:45 +0300

    Add solution for Time Needed to Buy Tickets

---
+++ leetcode/time-needed-to-buy-tickets.py
@@ -0,0 +1,15 @@
+class Solution:
+    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+
+        queue=deque(range(len(tickets)))
+
+        ans=0
+
+        while tickets[k]:
+            ind=queue.popleft()
+            tickets[ind]-=1
+            ans+=1
+            if tickets[ind]>0:
+                queue.append(ind)
+        return ans 
+        
