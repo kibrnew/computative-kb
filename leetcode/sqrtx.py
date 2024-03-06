bdd178fc48f9dcd0974324e6a22ec6a34f3e203e
kibrnew <123804824+kibrnew@users.noreply.github.com>
2024-03-06 16:55:49 +0300

    Add solution for Sqrt(x)

---
+++ leetcode/sqrtx.py
@@ -0,0 +1,22 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+
+        l=0
+        r=x
+        while l<=r:
+            mid=l+(r-l)//2
+
+            if mid*mid<=x:
+                l=mid+1
+            else:
+                r=mid-1
+
+        return r
+
+
+
+
+        
+            
+            
+        
